e7c4f5588dba442ccad13c1d5afdc06310f78be1
raptor <raptor@0xdeadbeef.info>
2024-08-24 16:12:58 +0200

    Another tracing iteration

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,23 @@
 use zero2prod::configuration::get_config;
 use zero2prod::startup::run;
 
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Initialize logging
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stderr);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
 
     // Get settings
     let config = get_config().expect("Failed to read configuration");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 /// Web form data
@@ -13,13 +14,15 @@ pub struct FormData {
 /// Subscriptions handler
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {request_id} - Adding '{}' '{}' as a new subscriber",
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
+    let _request_span_guard = request_span.enter();
 
-    tracing::info!("request_id {request_id} -Saving new subscriber details in the database");
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
@@ -27,15 +30,16 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
         form.name,
         Utc::now()
     )
-    .execute(db_pool.get_ref())
+    .execute(db_pool.as_ref())
+    .instrument(query_span)
     .await
     .map_or_else(
         |e| {
-            tracing::error!("request_id {request_id} -Failed to execute query: {e:?}");
+            tracing::error!("request_id {request_id} - Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         },
         |_| {
-            tracing::info!("request_id {request_id} -New subscriber details have been saved");
+            tracing::info!("request_id {request_id} - New subscriber details have been saved");
             HttpResponse::Ok().finish()
         },
     )
