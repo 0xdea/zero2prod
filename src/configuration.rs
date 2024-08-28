58c7815f105f584997fa0cb0dd42c01250451d60
raptor <raptor@0xdeadbeef.info>
2024-08-28 09:12:39 +0200

    Add environment variable based configuration and update dependencies

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,4 @@
-use config::{Config, ConfigError, File};
+use config::{Config, ConfigError, Environment, File};
 use secrecy::{ExposeSecret, Secret};
 
 /// Settings
@@ -26,22 +26,22 @@ pub struct DatabaseSettings {
 }
 
 /// Possible runtime environments
-pub enum Environment {
+pub enum Env {
     Development,
     Production,
 }
 
-impl Environment {
-    /// Represent Environment as a string
+impl Env {
+    /// Represent Env as a string
     pub fn as_str(&self) -> &'static str {
         match self {
-            Environment::Development => "dev",
-            Environment::Production => "prod",
+            Env::Development => "dev",
+            Env::Production => "prod",
         }
     }
 }
 
-impl TryFrom<String> for Environment {
+impl TryFrom<String> for Env {
     type Error = String;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
@@ -61,15 +61,20 @@ pub fn get_config() -> Result<Settings, ConfigError> {
     let config_dir = path.join("config");
 
     // Detect the running environment (default: `dev`)
-    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+    let env: Env = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "dev".into())
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT");
 
-    // Read the configuration files
+    // Read the configuration from files and environment variables
     Config::builder()
+        // Base configuration file
         .add_source(File::from(config_dir.join("base.yaml")).required(true))
-        .add_source(File::from(config_dir.join(environment.as_str())).required(true))
+        // Environment-specific configuration file
+        .add_source(File::from(config_dir.join(env.as_str())).required(true))
+        // Environment variables (e.g., `ZERO2PROD__APPLICATION__APP_PORT=8888`
+        // would set Settings.application.app_port to 8888)
+        .add_source(Environment::with_prefix("zero2prod").separator("__"))
         .build()?
         .try_deserialize()
 }
