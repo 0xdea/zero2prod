fb7f369e9798d3ebd709c2986a63f6d4bcb4d445
raptor <raptor@0xdeadbeef.info>
2024-09-17 14:02:19 +0200

    Continue implementing the newsletters endpoint

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,10 @@
-use actix_web::{web, HttpResponse};
+use std::fmt;
+
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
+use sqlx::PgPool;
+
+use crate::routes::helpers::error_chain_fmt;
 
 /// Newsletter data
 #[derive(serde::Deserialize)]
@@ -14,7 +20,55 @@ pub struct Content {
     text: String,
 }
 
+/// Confirmed subscriber data
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+/// Publish error type
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl fmt::Debug for PublishError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
 /// Newsletters handler to publish newsletters
-pub async fn newsletters(_newsletter: web::Json<NewsletterData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn newsletters(
+    newsletter: web::Json<NewsletterData>,
+    db_pool: web::Data<PgPool>,
+) -> Result<HttpResponse, PublishError> {
+    let subscribers = get_confirmed_subscribers(&db_pool).await?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+/// Get the list of confirmed subscribers
+#[tracing::instrument(name = "Get confirmed subscribers", skip(db_pool))]
+async fn get_confirmed_subscribers(
+    db_pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r"
+        SELECT email FROM subscriptions
+        WHERE status = 'confirmed'
+        "
+    )
+    .fetch_all(db_pool)
+    .await?;
+
+    Ok(rows)
 }
