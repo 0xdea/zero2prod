ce7c6e6200b0793d597eac94742c9c1a254f64da
raptor <raptor@0xdeadbeef.info>
2024-09-24 17:40:05 +0200

    Add redirect after login

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,9 @@
+use actix_web::http::header::LOCATION;
 use actix_web::HttpResponse;
 
 /// Login POST handler
 pub async fn login() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
 }
