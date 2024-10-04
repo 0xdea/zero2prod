6abc983d180d242c69e67d86b446f06aa367981d
raptor <raptor@0xdeadbeef.info>
2024-10-04 16:21:17 +0200

    Add SQL migration to populate the seed admin user with default credentials

--- migrations/20241004094617_seed_user.sql
+++ migrations/20241004094617_seed_user.sql
@@ -1,1 +1,4 @@
--- Add migration script here
+INSERT INTO users (user_id, username, password_hash)
+VALUES ('fa098efa-cb51-451e-acad-cea6dc5fbd48',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$pJIEELSC5EUo2Q1XTB+AFQ$mlv/OfE8ZuxRMLHYBx8PxcptIXr/SUF6VoUAOk+WJns');
