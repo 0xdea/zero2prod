2d02b4bfd42df8061bfee141d02c0dc95c59f95e
raptor <raptor@0xdeadbeef.info>
2024-10-06 17:29:48 +0200

    Continue implementing actix_web middleware to reject unauthenticated users

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,12 +1,30 @@
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::error::InternalError;
 use actix_web::middleware::Next;
+use actix_web::FromRequest;
 
-/*
+use crate::session_state::TypedSession;
+use crate::utils::{err500, see_other};
+
+/// Reject users that are not logged in
+#[allow(clippy::future_not_send)]
 pub async fn reject_unauth_users(
-    mut request: ServiceRequest,
+    mut req: ServiceRequest,
     next: Next<impl MessageBody>,
 ) -> actix_web::Result<ServiceResponse<impl MessageBody>> {
-    Ok(())
+    // Retrieve session
+    let session = {
+        let (http_request, payload) = req.parts_mut();
+        TypedSession::from_request(http_request, payload).await
+    }?;
+
+    // Check if the session state contains a `user_id`
+    if session.get_user_id().map_err(err500)?.is_some() {
+        next.call(req).await
+    } else {
+        let response = see_other("/login");
+        let e = anyhow::anyhow!("The user is not logged in");
+        Err(InternalError::from_response(e, response).into())
+    }
 }
-*/
